7fa468c4e337e94c71c5f3921be31580da54391e
volko <volkovwladislav@mail.ru>
2020-08-25 14:36:26 +0400

    2. Без переопределения equals и hashCode [#344667]

--- chapter_001/src/main/java/ru/job4j/list/User.java
+++ chapter_001/src/main/java/ru/job4j/list/User.java
@@ -19,7 +19,7 @@ public class User {
     public static void main(String[] args) {
         User first = new User("Vlad", 0, new GregorianCalendar(1992, 3, 7));
         User second = new User("Vlad", 0, new GregorianCalendar(1992, 3, 7));
-        Map<User, String> map = new HashMap<>();
+        Map<User, Object> map = new HashMap<>();
         map.put(first, "first");
         map.put(second, "second");
         System.out.println(map);
