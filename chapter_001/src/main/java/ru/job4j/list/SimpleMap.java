8d28a1565bd941ca5dfae7deb62375a6b44ecbe7
volko <volkovwladislav@mail.ru>
2020-08-27 19:43:46 +0400

    8. Реализовать собственную структуру данных - HashMap [#344670]

--- chapter_001/src/main/java/ru/job4j/list/SimpleMap.java
+++ chapter_001/src/main/java/ru/job4j/list/SimpleMap.java
@@ -3,10 +3,13 @@ package ru.job4j.list;
 import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
+import java.util.Objects;
 
-public class SimpleMap<K, V> implements Iterable<V> {
+public class SimpleMap<K, V> implements Iterable<K> {
     private int size;
     private int modCount;
+
+    private final double loadFactor = 0.75;
 
     public class Node<K, V> {
         K key;
@@ -23,13 +26,13 @@ public class SimpleMap<K, V> implements Iterable<V> {
     private Node<K, V>[] date = new Node[16];
 
     public boolean insert(K key, V value) {
-        if (size == date.length) {
+        if (size >= date.length * loadFactor) {
             plusSize();
         }
         int hash = hash(key);
         int i = hash & (date.length - 1);
         var t = date[i];
-        if (t != null && t.key.equals(key)) {
+        if (t != null && Objects.equals(t.key, key)) {
             t.value = value;
             modCount++;
             return true;
@@ -46,7 +49,7 @@ public class SimpleMap<K, V> implements Iterable<V> {
     public V get(K key) {
         int i = getIndex(key.hashCode());
         var t = date[i];
-        if (t != null && t.key.equals(key)) {
+        if (t != null && Objects.equals(t.key, key)) {
             return t.value;
         }
         return null;
@@ -55,7 +58,7 @@ public class SimpleMap<K, V> implements Iterable<V> {
     public boolean delete(K key) {
         int i = getIndex(key.hashCode());
         var t = date[i];
-        if (t != null && t.key.equals(key)) {
+        if (t != null && Objects.equals(t.key, key)) {
             date[i] = null;
             size--;
             modCount++;
@@ -76,13 +79,15 @@ public class SimpleMap<K, V> implements Iterable<V> {
         var oldDate = date;
         date = new Node[date.length * 2];
         for (var a: oldDate) {
-            date[getIndex(a.hash)] = a;
+            if (a != null) {
+                date[getIndex(a.hash)] = a;
+            }
         }
     }
 
     @Override
-    public Iterator<V> iterator() {
-        return new Iterator<V>() {
+    public Iterator<K> iterator() {
+        return new Iterator<K>() {
             private int s = 0;
             private int in = 0;
             private final int expectedModCount = modCount;
@@ -93,7 +98,7 @@ public class SimpleMap<K, V> implements Iterable<V> {
             }
 
             @Override
-            public V next() {
+            public K next() {
                 if (expectedModCount != modCount) {
                     throw new ConcurrentModificationException();
                 }
@@ -101,10 +106,9 @@ public class SimpleMap<K, V> implements Iterable<V> {
                     throw new NoSuchElementException();
                 }
                 while (in < date.length) {
-                    if (date[in] != null) {
-                        in++;
+                    if (date[in++] != null) {
                         s++;
-                        return date[in].value;
+                        return date[in].key;
                     }
                 }
                 return null;
