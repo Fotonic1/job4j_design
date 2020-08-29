6524a3dbdecad4d4559770982c1166615c37ca8a
volko <volkovwladislav@mail.ru>
2020-08-29 12:49:34 +0400

    1. Читаем файл конфигурации [#344689]

---
+++ chapter_002/src/main/java/ru/job4j/io/Config.java
@@ -0,0 +1,45 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
+import java.util.stream.Collectors;
+
+public class Config {
+    private final String path;
+    private final Map<String, String> values = new HashMap<>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    public void load() {
+        try (BufferedReader in = new BufferedReader(new FileReader(path))) {
+            in.lines().filter(s -> !s.equals("") && !s.startsWith("##")).map(s -> s.split("=")).forEach(strings -> values.put(strings[0], strings[1]));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String value(String key) {
+        return values.get(key);
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines().forEach(out::add);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return out.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Config("app.properties"));
+    }
+}
