8839c0de26c6a359122d74148b37834c08da2d3d
volko <volkovwladislav@mail.ru>
2020-08-30 16:54:15 +0400

    3.0. Тестирование IO [#344681]

--- chapter_002/src/test/java/ru/job4j/io/AnalizeTest.java
+++ chapter_002/src/test/java/ru/job4j/io/AnalizeTest.java
@@ -5,8 +5,6 @@ import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
 import java.io.*;
-import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
