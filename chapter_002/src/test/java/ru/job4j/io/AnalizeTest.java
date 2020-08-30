46fc1e3f4aa87ce438665b60afea2da2c1bf5c70
volko <volkovwladislav@mail.ru>
2020-08-30 16:54:03 +0400

    3.0. Тестирование IO [#344681]

--- chapter_002/src/test/java/ru/job4j/io/AnalizeTest.java
+++ chapter_002/src/test/java/ru/job4j/io/AnalizeTest.java
@@ -1,9 +1,10 @@
 package ru.job4j.io;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.*;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,14 +12,29 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 public class AnalizeTest {
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
     @Test
-    public void unavailable() {
+    public void unavailable() throws IOException {
+        File source = folder.newFile("source.txt");
+        File target = folder.newFile("target.txt");
+        try (FileWriter out = new FileWriter(source)){
+            out.write("200 10:56:01\n" +
+                    "500 10:57:01\n" +
+                    "400 10:58:01\n" +
+                    "200 10:59:01\n" +
+                    "500 11:01:02\n" +
+                    "200 11:02:02");
+        }
         Analize analize = new Analize();
-        analize.unavailable("./data/test_log.txt", "./data/test_un.txt");
-        try (BufferedReader in = new BufferedReader(new FileReader("./data/test_un.txt"))){
-            assertThat(in.lines().collect(Collectors.toList()),is(List.of("10:57:01;10:59:01", "11:01:02;11:02:02")));
+        analize.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        StringBuilder rsl = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new FileReader(target))){
+           in.lines().forEach(rsl::append);
         } catch (Exception e){
             e.printStackTrace();
         }
+        assertThat(rsl.toString(),is("10:57:01;10:59:0111:01:02;11:02:02"));
     }
 }
