4f4c3fb1d9915aa830958963bc5b9cbf7189f843
volko <volkovwladislav@mail.ru>
2020-08-30 17:35:04 +0400

    4.0. File [#344686]

--- chapter_002/src/test/java/ru/job4j/io/AnalizeTest.java
+++ chapter_002/src/test/java/ru/job4j/io/AnalizeTest.java
@@ -17,22 +17,22 @@ public class AnalizeTest {
     public void unavailable() throws IOException {
         File source = folder.newFile("source.txt");
         File target = folder.newFile("target.txt");
-        try (FileWriter out = new FileWriter(source)){
-            out.write("200 10:56:01\n" +
-                    "500 10:57:01\n" +
-                    "400 10:58:01\n" +
-                    "200 10:59:01\n" +
-                    "500 11:01:02\n" +
-                    "200 11:02:02");
+        try (FileWriter out = new FileWriter(source)) {
+            out.write("200 10:56:01\n"
+                    + "500 10:57:01\n"
+                    + "400 10:58:01\n"
+                    + "200 10:59:01\n"
+                    + "500 11:01:02\n"
+                    + "200 11:02:02");
         }
         Analize analize = new Analize();
         analize.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
         StringBuilder rsl = new StringBuilder();
-        try (BufferedReader in = new BufferedReader(new FileReader(target))){
+        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
            in.lines().forEach(rsl::append);
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        assertThat(rsl.toString(),is("10:57:01;10:59:0111:01:02;11:02:02"));
+        assertThat(rsl.toString(), is("10:57:01;10:59:0111:01:02;11:02:02"));
     }
 }
