baedf6ab669dbccb278667e0b8e77717246e9b3e
volko <volkovwladislav@mail.ru>
2020-08-31 12:14:40 +0400

    4.1. Сканирование файловой системы. [#344680]

---
+++ chapter_002/src/main/java/ru/job4j/io/Search.java
@@ -0,0 +1,21 @@
+package ru.job4j.io;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.List;
+
+
+public class Search {
+    public static void main(String[] args) throws IOException {
+        Path start = Paths.get(".");
+        search(start, "js").forEach(System.out::println);
+    }
+
+    public static List<Path> search(Path root, String ext) throws IOException {
+        SearchFiles seacher = new SearchFiles(p -> p.toFile().getName().endsWith(ext));
+        Files.walkFileTree(root, seacher);
+        return seacher.getPaths();
+    }
+
+}
