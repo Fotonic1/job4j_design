02d1d5fca294359a60a5c4cec3468c26172a1939
volko <volkovwladislav@mail.ru>
2020-09-09 22:36:38 +0400

    Хранилище машин [#344621]

--- chapter_003/all_car_select.sql
+++ chapter_003/all_car_select.sql
@@ -4,6 +4,6 @@ e.name as engine,
 tr.name as transmission,
 cc.name as carcase
 from car
-left outer join engine as e on car.engine_id = e.id
-left outer join transmission as tr on car.transmission_id = tr.id
-left outer join carcase as cc on car.carcase_id = cc.id
+inner join engine as e on car.engine_id = e.id
+inner join transmission as tr on car.transmission_id = tr.id
+inner join carcase as cc on car.carcase_id = cc.id
