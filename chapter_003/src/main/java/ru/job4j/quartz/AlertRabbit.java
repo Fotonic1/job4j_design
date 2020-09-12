388fe74b34fb310678d1b3225c10f19beeee6f70
volko <volkovwladislav@mail.ru>
2020-09-12 16:35:00 +0400

    1. Quartz [#344599]

---
+++ chapter_003/src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -0,0 +1,44 @@
+package ru.job4j.quartz;
+
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+
+import java.io.FileReader;
+import java.util.Properties;
+
+import static org.quartz.JobBuilder.*;
+import static org.quartz.SimpleScheduleBuilder.*;
+import static org.quartz.TriggerBuilder.*;
+
+public class AlertRabbit {
+    public static void main(String[] args) {
+        Properties cfg = new Properties();
+        try (FileReader in = new FileReader("./chapter_003/src/main/resources/rabbit.properties")) {
+            cfg.load(in);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+            scheduler.start();
+            JobDetail job = newJob(Rabbit.class).build();
+            SimpleScheduleBuilder times = simpleSchedule()
+                    .withIntervalInSeconds(Integer.parseInt(cfg.getProperty("rabbit.interval")))
+                    .repeatForever();
+            Trigger trigger = newTrigger()
+                    .startNow()
+                    .withSchedule(times)
+                    .build();
+            scheduler.scheduleJob(job, trigger);
+        } catch (SchedulerException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static class Rabbit implements Job {
+        @Override
+        public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+            System.out.println("Rabbit runs here ...");
+        }
+    }
+}
